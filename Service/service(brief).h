7bb8f9eb0a71608dd90bcf752662d21708b8cf9b
TAK1ST <90909909+TAK1ST@users.noreply.github.com>
2024-04-25 10:43:24 +0700

    Add files via upload

---
+++ Service/service(brief).h
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX_SERVICES 100
+#define MAX_NAME_LENGTH 50
+
+void displayServices();
+void addService();
+void editService();
+void deleteService();
